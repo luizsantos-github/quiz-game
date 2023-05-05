c127fcdc27e706420be301c53c20f33b674347bf
Luiz Santos <luizsantos.github@gmail.com>
2023-05-05 17:59:03 +0800

    Complete quiz project

---
+++ data.py
@@ -0,0 +1,37 @@
+question_data = [
+    {
+        "category": "Entertainment: Japanese Anime & Manga",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In the 1988 film &quot;Akira&quot;, Tetsuo ends up destroying Tokyo.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "Clefairy was intended to be Ash&#039;s starting Pok&eacute;mon in the pilot episode of the cartoon.",
+     "correct_answer": "True", "incorrect_answers": ["False"]
+     },
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "In Kill La Kill, the weapon of the main protagonist is a katana. ", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "The anime &quot;Lucky Star&quot; follows the story of one girl who is unaware she is God.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "No Game No Life first aired in 2014.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "Gosho Aoyama Made This Series: (Detective Conan \/ Case Closed!)", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "In the &quot;Melancholy of Haruhi Suzumiya&quot; series, the narrator goes by the nickname Kyon.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "In Pok&eacute;mon, Ash&#039;s Pikachu refuses to go into a pokeball.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "In the &quot;Toaru Kagaku no Railgun&quot; anime,  espers can only reach a maximum of level 6 in their abilities.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "easy",
+     "question": "In Chobits, Hideki found Chii in his apartment.", "correct_answer": "False",
+     "incorrect_answers": ["True"]}
+]
